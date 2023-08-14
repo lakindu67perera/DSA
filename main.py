7516afb4c4fcfa1f57ccd6c8ce587072c783f213
lakindu67perera <136733109+lakindu67perera@users.noreply.github.com>
2023-08-14 10:29:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+text=input("enter the text:- ")
+pattern = input("enter the pattern:- ")
+
+
+
+text_list=list(text)
+pattern_list=list(pattern)
+text_len= len(text_list)
+pattern_len=len(pattern_list)
+print(text_len)
+print(pattern_len)
+
